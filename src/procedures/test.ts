656c83fb50b092337bff5834e37d269039abfe12
Corno Schraverus <corno@schraverus.com>
2025-09-30 19:35:17 +0200

    .

---
+++ src/procedures/test.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import * as _ea from 'exupery-core-alg'
+import * as _eb from 'exupery-core-bin'
+import * as _easync from 'exupery-core-async'
+
+import { $$ as cmd_log_error } from "exupery-resources/dist/actions/log_error"
+
+export const $$: _eb.Run_Unguaranteed_Procedure_Main = ($i) => $i.execute(() => cmd_log_error(_ea.array_literal([
+    "No tests yet"
+])))
---
+++ src/test.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import * as _eb from 'exupery-core-bin'
+
+import { $$ } from "./procedures/test"
+
+_eb.run_unguaranteed_procedure($$)
